27076d9c24d894c4b84dcde09b98ed8918a6b172
Jakub Hozak <jakub.hozak@gmail.com>
2012-12-12 12:54:33 +0100

    stuff

--- jakub/registers.el
+++ jakub/registers.el
@@ -1,6 +1,6 @@
 (set-register ?e '(file . "~/.emacs.d/init.el"))
 (set-register ?b '(file . "~/sh"))
-(set-register ?r '(file . "~/prog/3scale/remonster/config/routes.rb"))
+(set-register ?r '(file . "~/prog/3scale/system/config/routes.rb"))
 (set-register ?h '(file . "/sudo:localhost:/etc/hosts"))
 
 (set-register ?a '(file . "/bender@aljazeera.3scale.net:/home/bender/system_edge/current/"))
