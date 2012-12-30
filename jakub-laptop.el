ae186ca9dd30c499c89b860752826606c7d112a1
Jakub Hozak <jakub.hozak@gmail.com>
2012-12-30 20:29:23 +0100

    stuff

--- jakub-laptop.el
+++ jakub-laptop.el
@@ -1,2 +1,13 @@
 (load-theme 'tango-dark)
+
+
+(let ((class '((class color) (min-colors 89))))
+  (custom-theme-set-faces
+   'tango-dark
+   ;; Highlighting faces
+   `(highlight ((,class (:foreground unspecified :background "#222")))))
+  )
+
+
+
 (set-frame-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1")
